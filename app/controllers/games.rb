5a437f7bc0d045f68ad91e1c721570a40917d59f
milfred <jasonmilfred@gmail.com>
2016-06-11 13:12:57 -0500

    Add gitignore file to hide api key

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -1,6 +1,6 @@
 post "/games" do
   if request.xhr?
-    word_xml = open("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/#{params[:word]}?key=9a3ee4cd-063d-4bbd-903f-908220282f3f")
+    word_xml = open("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/#{params[:word]}?key=#{api_key}")
     response_body = word_xml.read[559, 441]
     p response_body
     if response_body
